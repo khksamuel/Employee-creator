8172a4496ebad10619b07e9aef9c69bb2aa72926
khksamuel <a1821027@student.adelaide.edu.au>
2026-08-18 19:43:58 +0930

    add Hero component with styling for employee list

---
+++ employee-creator-web/src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import styles from './Hero.module.scss';
+
+function Hero() {
+  return (
+    <div className={styles.hero}>
+            <h1 className={styles.heroTitle}>Employee's List</h1>
+    </div>
+  );
+}
+
+export default Hero;
