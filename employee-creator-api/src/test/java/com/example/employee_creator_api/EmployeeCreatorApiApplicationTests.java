5724398f33463f3c06d4e2e7cd8b2b43863c1fad
khksamuel <a1821027@student.adelaide.edu.au>
2026-08-18 16:42:05 +0930

    initialise project with vite and spring

---
+++ employee-creator-api/src/test/java/com/example/employee_creator_api/EmployeeCreatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_creator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCreatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
