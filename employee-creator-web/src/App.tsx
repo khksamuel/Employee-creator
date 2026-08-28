1c615973e94d7ff2b3fc98ad3e2747e8d815ac9b
khksamuel <a1821027@student.adelaide.edu.au>
2026-08-28 12:34:10 +0930

    mirgrate from original vite design styles

--- employee-creator-web/src/App.tsx
+++ employee-creator-web/src/App.tsx
@@ -1,17 +1,20 @@
-import './App.css'
-import Hero from './components/Hero/Hero'
-import AddEmployee from './components/AddEmployee/AddEmployee'
-import EmployeeList from './components/EmployeeList/EmployeeList'
-
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import AddEmployee from "./components/AddEmployee/AddEmployee";
+import EmployeeList from "./components/EmployeeList/EmployeeList";
 
 function App() {
   return (
-    <>
+    <div className="appShell">
       <Hero />
-      <AddEmployee />
-      <EmployeeList />
-    </>
-  )
+      <main className="workspace">
+        <AddEmployee />
+        <section className="employeeDirectory" aria-label="Employee directory">
+          <EmployeeList />
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
